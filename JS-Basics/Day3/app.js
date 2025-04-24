bf4b7f32bd574b879ec016db9a0f07b169ed256e
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-04-24 16:49:52 +0530

    first commit

---
+++ JS-Basics/Day3/app.js
@@ -0,0 +1,110 @@
+// console.log("Hello world")
+// console.log(1)
+// console.log(true)
+// console.log(undefined)
+// console.log(null)
+
+
+
+// var name // declaration
+// const age = 32 // declaration and initialisation
+// let isHandsome = true
+// name = "Gabbar" // initialisation
+
+
+// console.log(name)
+// console.log(age)
+// console.log(isHandsome)
+
+
+
+
+
+// var a
+// var a 
+
+// a = 20
+// console.log(a)
+
+// a = true
+// console.log(a)
+
+
+
+// let b = 10
+// let b = "abc"
+
+
+// let a = 19
+// a = false
+// console.log(a)
+
+
+// var a
+// let b
+
+// const c = 99
+// c = "jhsdbfshgd"
+
+
+
+// Strings
+
+// let str = "My name is ABC"
+// console.log(str)
+// console.log(typeof(str))
+
+
+// let newStr = str.toLowerCase()
+
+
+// console.log(str)
+// console.log(newStr)
+
+
+// console.log(str.toLowerCase())
+// let str = "My name is ABC"
+// console.log(str.toUpperCase())
+// console.log(str.length)
+// console.log(str.charAt(5))
+// console.log(str[100])
+// console.log(str[str.length - 1])
+
+
+
+// let a = "alakazam"
+
+// let newValue = a.replace("a", "o")
+
+// let newValue = a.replaceAll("a", "o")
+
+
+// console.log(a ,newValue)
+// console.log(a)
+// console.log(newValue)
+
+
+
+let str = "qwertyuiop"
+console.log(str.includes("T"))
+console.log(str.includes("t"))
+console.log(str.concat("  ").concat("hehe"))
+console.log(str.indexOf("qwe"))
+console.log(str.indexOf("ty"))
+console.log(str.indexOf("qwes"))
+console.log(str.indexOf("z"))
+console.log(str.indexOf("ytrewq"))
+
+
+let str2 = "I am learning Javascript"
+let ans = str2.slice(7)
+let ans2 = str2.slice(7, 12)
+let ans3 = str2.slice(-5)
+let ans4 = str2.slice(-8,-5)
+
+
+console.log(str2)
+console.log(ans, ans2, ans3, ans4)
+
+
+
