519ffb8d6487a27db076c0f4b8dc467292c25292
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-04-25 16:56:13 +0530

    Commit

---
+++ JS-Basics/Day4/Operators/app.js
@@ -0,0 +1,149 @@
+// let val1 = 12
+// let val2 = 37
+// let ans = val1 + val2
+// let ans = val1 - val2
+// let ans = val1 * val2
+// let ans = 10 / 2
+// let ans = 10 % 2
+// let ans = 2 ** 6
+// let ans = 49 ** (1/2)
+// console.log(ans)
+
+
+
+// let a = 1
+// a++
+// ++a
+// console.log(a)
+
+
+// let a = 1
+// console.log(a++)
+// // console.log(a)
+// console.log(++a)
+
+
+// let a = 1
+// console.log(a--)
+// console.log(--a)
+// console.log(a)
+
+
+// let a = 10
+// let b = a++
+// let b = ++a
+// console.log(b)
+
+
+// let a = 10
+// let b = 20
+
+// let a = "abc"
+// let b = "xyz"
+
+// let a = false
+// let b = false
+
+// console.log(a == b)
+
+
+
+
+// let a = 10
+// let b = "10"
+
+// console.log(a === b)
+
+// let a = 10
+// let b = 10
+
+
+// let a = 10
+// let b = "10"
+
+
+// console.log(a != b)
+// console.log(a !== b)
+
+
+// let a = 10
+// let b = 10
+
+
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a >= b)
+
+
+
+
+
+
+// console.log(true || true)
+// console.log(false || false)
+// console.log(true || false)
+// console.log(false || true)
+
+
+
+// console.log(true && true)
+// console.log(false && true)
+// console.log(true && false)
+// console.log(false && false)
+
+
+// let a = 10
+// let b = 20
+// console.log(a < b || a > b)
+// console.log(a < b && a > b)
+
+
+
+//short circuiting
+// let a = 22
+// let b = 37
+
+
+// a < b || a > b || a == b
+
+
+// true && console.log("OKAY")
+// false && console.log("OKAY")
+
+
+
+
+
+// let a = 10
+// a = a + 5
+// a+=5 // a = a + 5
+// a-=5 // a = a - 5
+// a*=5 // a = a * 5
+// a/=5 // a = a / 5
+// a%=5 // a = a % 5
+// console.log(a)
+
+
+// console.log(5 - 10 / 2 + 5 * 3)
+
+
+// console.log("Humpty " + "Sharma")
+// console.log("Humpty" + " Sharma")
+// console.log("Humpty" + " " + "Sharma")
+// console.log(1 + 1)
+// console.log("1" + 1)
+
+
+console.log(10 + "10") // 1010
+console.log(10 + 10 + "10") // 2010
+console.log("10" + 10 + 10) // 101010
+console.log(10 + "10" + 10) // 101010
+console.log("10" + 10 + "10") // 101010
+console.log(true + 10)
+console.log(true + true)
+console.log(true + "" + true)
+console.log(1 + true + "1")
+console.log("1" + true + 1)
+
+
---
+++ JS-Basics/Day4/Typecasting/app.js
@@ -0,0 +1,22 @@
+// let num = 9315
+//console.log(typeof(num))
+// let str = String(num)
+// console.log(typeof(str))
+// console.log(num, str)
+
+
+
+// let str = "7819"
+// // let str = "abcd"
+// console.log(typeof(str))
+// let num = Number(str)
+// console.log(typeof(num))
+// console.log(str, num)
+
+
+
+let a = false
+console.log(typeof(a))
+console.log(typeof(String(a)))
+console.log(typeof(Number(a)))
+console.log(Number(a))
