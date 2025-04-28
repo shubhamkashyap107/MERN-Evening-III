ba0e5d5772c94574c1eaa8db53d8fcdbf008518a
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-04-28 16:14:21 +0530

    commit

---
+++ JS-Basics/Day5/conditionals/app.js
@@ -0,0 +1,130 @@
+
+// let num1 = Number(prompt("Please enter number 1"))
+// let num2 = Number(prompt("Please enter number 2"))
+
+// console.log(num1 + num2)
+
+
+
+
+// let age = Number(prompt("Please enter your age"))
+// let age = prompt("Please enter your age")
+
+
+// if(age > 18)
+// {
+//     console.log("OKAY")
+// }
+
+
+// let n = prompt("Please enter your number")
+
+
+// if(n % 5 == 0)
+// {
+//     console.log("Divisible by 5")
+// }
+// else
+// {
+//     console.log("Not divisible by 5")
+// }
+
+
+// let n = prompt("Please enter your number")
+
+
+// if(n % 2 == 0)
+// {
+//     console.log("Even")
+// }
+// else
+// {
+//     console.log("Odd")
+// }
+
+
+// let n = prompt("Please enter your number")
+
+// if(n == 0)
+// {
+//     console.log("Neutral")
+// }
+// else if(n > 0)
+// {
+//     console.log("Positive")
+// }
+// else
+// {
+//     console.log("Negative")
+// }
+
+
+
+
+// let age = prompt("Please enter your age")
+
+// if(age > 0 && age <= 12)
+// {
+//     console.log("Kid")
+// }
+// else if(age >= 13 && age <= 19)
+// {
+//     console.log("Teenager")
+// }
+// else if(age >= 20 && age <= 64)
+// {
+//     console.log("Adult")
+// }
+// else
+// {
+//     console.log("Senior Citizen")
+// }
+
+// let m1 = Number(prompt("Enter marks 1"))
+// let m2 = Number(prompt("Enter marks 2"))
+// let m3 = Number(prompt("Enter marks 3"))
+// let m4 = Number(prompt("Enter marks 4"))
+// let m5 = Number(prompt("Enter marks 5"))
+// let avg = (m1 + m2 + m3 + m4 + m5) / 5
+
+// if(avg <= 100 && avg >= 91)
+// {
+//     console.log("A")
+// }
+// else if(avg < 91 && avg >= 81)
+// {
+//     console.log("B")
+// }
+// else if(avg < 81 && avg >= 71)
+// {
+//     console.log("C")
+// }
+// else if(avg < 71 && avg >= 61)
+// {
+//     console.log("D")
+// }
+// else
+// {
+//     console.log("Fail")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
