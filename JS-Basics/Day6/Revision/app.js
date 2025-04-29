e7ab2812af275d5f03d688edae66f043f3fcb9b8
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-04-29 16:48:10 +0530

    Commit

---
+++ JS-Basics/Day6/Revision/app.js
@@ -0,0 +1,148 @@
+// let year = Number(prompt("Enter the year"))
+
+// if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+// {
+//     console.log("Leap year")
+// }
+// else
+// {
+//     console.log("Not a leap year")
+// }
+
+
+
+// let day = Number(prompt("Enter the day"))
+
+
+// if(day == 1)
+// {
+//     console.log("Monday")
+// }
+// else if(day == 2)
+// {
+//     console.log("Tuesday")
+// }
+// else if(day == 3)
+// {
+//     console.log("Wednesday")
+// }
+// else if(day == 4)
+// {
+//     console.log("Thursday")
+// }
+// else if(day == 5)
+// {
+//     console.log("Friday")
+// }
+// else if(day == 6)
+// {
+//     console.log("Saturday")
+// }
+// else if(day == 7)
+// {
+//     console.log("Sunday")
+// }
+
+
+// let month = Number(prompt("ENter the month"))
+
+
+// if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 )
+// {
+//     console.log(31)
+// }
+// else if(month == 4 || month == 6 || month == 9 || month == 11)
+// {
+//     console.log(30)
+// }
+// else
+// {
+//     console.log(28)
+// }
+
+
+
+
+
+
+// let num = -28
+
+// if(num < 0 && num % 2 == 0)
+// {
+//     console.log("negative, even")
+// }
+// else if(num < 0 && num % 2 != 0)
+// {
+//     console.log("Negative, odd")
+// }
+// else if(num > 0 && num % 2 == 0)
+// {
+//     console.log("Positive, even")
+// }
+// else
+// {
+//     console.log("Postice, Odd")
+// }
+
+
+
+
+
+
+
+// if(num < 0)
+// {
+//     if(num % 2 == 0)
+//     {
+//         console.log("Negative, Even")
+//     }
+//     else 
+//     {
+//         console.log("Negative, Odd")
+//     }
+// }
+// else if(num > 0)
+// {
+//     if(num % 2 == 0)
+//     {
+//         console.log("Positive, Even")
+//     }
+//     else 
+//     {
+//         console.log("Positive, Odd")
+//     }
+// }
+
+
+
+
+
+let gender = prompt("Enter Gender")
+let age = Number(prompt("Please enter age"))
+
+
+
+if(gender == "M")
+{
+    if(age < 22)
+    {
+        alert("NO")
+    }
+    else
+    {
+        alert("YES")
+    }
+}
+else
+{
+    if(age < 18)
+    {
+        alert("NO")
+    }
+    else
+    {
+        alert("YES")
+    }
+}
+
+
---
+++ JS-Basics/Day6/TernaryOperator/app.js
@@ -0,0 +1,58 @@
+// let age = -999
+
+// age < 18 ? console.log("Underage") : console.log("Access granted")
+
+
+
+
+
+// age >= 1 && age <= 12 ? console.log("Kid") : 
+// (age >= 13 && age <= 19 ? console.log("Teenager") : 
+// (age >= 20 && age <= 64 ? console.log("Adult") : 
+// (age >= 65 ? console.log("Senior Citizen" ) : 
+// console.log("Invalid age")
+// ))) 
+
+
+
+// let marks = Number(prompt("Enter your marks"))
+
+
+
+// marks >= 91 && marks <= 100 ? console.log("A") : 
+// marks >= 81 && marks < 91 ? console.log("B") :
+// marks >= 71 && marks < 81 ? console.log("C") :
+// marks >= 61 && marks < 71 ? console.log("D") : 
+// console.log("FAIL")
+
+
+// let n1 = 2
+// let n2 = 32
+
+
+// n1 > n2 ? console.log(n1) : console.log(n2)
+
+
+
+
+let num = Number(prompt("Enter your number"))
+
+
+
+num < 0 ? (num % 2 == 0 ? console.log("Negative, Even") : console.log("Negative, Odd")) :
+(num % 2 == 0 ? console.log("Positive, Even") : console.log("Positive, Odd"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
