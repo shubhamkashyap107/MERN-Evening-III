26d34ee727590bf4596762de38de6e1353555de8
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-06-30 16:49:33 +0530

    COmmit

---
+++ Advanced JS/Day 1/app.js
@@ -0,0 +1,31 @@
+let element = document.getElementById("container")
+
+
+// let element = document.getElementsByClassName("xyz")
+// console.log(element[3])
+
+// for(let item of element)
+// {
+//     console.log(item)
+// }
+
+
+// let elements = document.getElementsByTagName("p")
+// console.log(elements)
+
+
+
+// let ele = document.querySelector("#container")
+// let ele = document.querySelector(".xyzabc")
+// let ele = document.querySelector("div")
+// console.log(ele)
+
+
+// let elements = document.querySelectorAll(".xyz")
+// let elements = document.querySelectorAll("#container")
+// let elements = document.querySelectorAll("p")
+// console.log(elements)
+
+
+
+// console.log(document)
