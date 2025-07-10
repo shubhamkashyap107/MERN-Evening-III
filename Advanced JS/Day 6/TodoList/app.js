78dad70bd29ade31ae8d4a46637b470a2ce1f8ad
Shubham Kashyap <shubhamkashyap107@gmail.com>
2025-07-10 15:01:12 +0530

    Commit

--- Advanced JS/Day 6/TodoList/app.js
+++ Advanced JS/Day 6/TodoList/app.js
@@ -2,7 +2,62 @@ const inputBar = document.getElementById("ip")
 const textAreaBar = document.getElementById("ta")
 const addTaskBtn = document.getElementById("btn")
 const rightSideDiv = document.getElementById("right")
+// localStorage.setItem("todos", JSON.stringify([]))
 
+let existingTodosStr = localStorage.getItem("todos")
+let existingTodos = JSON.parse(existingTodosStr)
+
+for(let item of existingTodos)
+{
+    const card = document.createElement("div")
+    const taskTitle = document.createElement("h2")
+    const taskDesc = document.createElement("p")
+    const textWrapper = document.createElement("div")
+    let deleteBtn = document.createElement("button")
+
+    card.setAttribute("id", "card")
+    taskTitle.setAttribute("id", "title")
+    taskDesc.setAttribute("id", "desc")
+    textWrapper.setAttribute("id", "tw")
+    deleteBtn.setAttribute("id", "delBtn")
+
+
+    deleteBtn.addEventListener("click", () => {
+        delTodo(item.title)
+        card.remove()
+    })
+
+    taskTitle.innerText = item.title
+    taskDesc.innerText = item.desc
+
+    textWrapper.appendChild(taskTitle)
+    textWrapper.appendChild(taskDesc)
+
+    card.appendChild(textWrapper)
+    deleteBtn.innerText = "🗑️"
+    card.appendChild(deleteBtn)
+
+    rightSideDiv.appendChild(card)
+
+}
+
+
+function delTodo(todoTitle)
+{
+    console.log(todoTitle)
+    let arr = JSON.parse(localStorage.getItem("todos"))
+    let temp = []
+
+    for(let item of arr)
+    {
+        if(item.title != todoTitle)
+        {
+            temp.push(item)
+        }
+    }
+
+    localStorage.setItem("todos", JSON.stringify(temp))
+}
 
 
 addTaskBtn.addEventListener("click", () => {
@@ -12,11 +67,30 @@ addTaskBtn.addEventListener("click", () => {
         return 
     }
     
+    let strArr = localStorage.getItem("todos")
+    let arr = JSON.parse(strArr)
+
+    let obj = {
+        title : inputBar.value,
+        desc : textAreaBar.value
+    }
+
+    arr.push(obj)
+    localStorage.setItem("todos", JSON.stringify(arr))
+
+
+
     const card = document.createElement("div")
     const taskTitle = document.createElement("h2")
     const taskDesc = document.createElement("p")
     const textWrapper = document.createElement("div")
     let deleteBtn = document.createElement("button")
+
+    deleteBtn.addEventListener("click", () => {
+        delTodo(inputBar.value)
+        card.remove()
+    })
+
 
     card.setAttribute("id", "card")
     taskTitle.setAttribute("id", "title")
@@ -40,3 +114,15 @@ addTaskBtn.addEventListener("click", () => {
     textAreaBar.value = ""
 
 }) // HOF
+
+
+
+// let deletBtns = document.querySelectorAll("#delBtn")
+
+
+// for(let item of deletBtns)
+// {
+//     item.addEventListener("click", () => {
+//         item.remove()
+//     })
+// }
